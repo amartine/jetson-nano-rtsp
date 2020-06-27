cff6e9087ba4faec5a1992774e24ca97d7f970f1
amartine <alejandro.martinez.falcon@gmail.com>
2020-06-27 01:36:05 -0400

    Create openCV-rtsp.py

---
+++ openCV-rtsp.py
@@ -0,0 +1,16 @@
+import cv2
+#print(cv2.__version__)
+
+camSet= 'rtsp://192.168.1.27:80/live/stream'
+cam = cv2.VideoCapture(camSet)
+
+cv2.namedWindow('Camera')
+cv2.moveWindow('Camera',800,400)
+
+while (True):
+    ret, frame= cam.read()
+    cv2.imshow('Camera', frame)
+    if (cv2.waitKey(1)==ord('q')):
+        break
+cam.release()
+cv2.destroyAllWindows()
